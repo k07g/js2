fa5969d03fed50c4a6987079b8edfe6071222386
k07g <155704074+k07g@users.noreply.github.com>
2026-04-02 22:50:08 +0900

    Promiseの結果を表示する (#6)

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import SuspenseSimple from './SuspenseSimple.jsx'
+import SuspenseResult from './SuspenseResult.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SuspenseSimple />
+    <SuspenseResult />
   </StrictMode>,
 )
